e9d31847b842d4e0ec38275869e58f0a31fa3012
Red Bin <binred@outlook.com>
2014-10-28 08:52:43 +0800

    fddfdfdfd

--- strategy_mining/model_build_price_series2.py
+++ strategy_mining/model_build_price_series2.py
@@ -20,32 +20,118 @@ logging.basicConfig(level = logging.DEBUG,
     format = '%(levelname)s %(asctime)s [%(filename)s:%(lineno)d] %(message)s',                                                                              
     filename = local_path + '/log/%s.log' % (os.path.basename(sys.argv[0]),), 
     filemode = 'a')
-def extract_features_and_classes(close_prices, window): #{{{
-    ret = ""
-    for i in range(len(close_prices)-window-1):
-        for  j in range(window):
-            inc = close_prices[i+j+1] * 1.0 / close_prices[i+j]
-            ret +=  str(inc) + ","
-        classes = 0
-        if close_prices[i+window + 1] > close_prices[i+window] :
-             classes = 1
-        ret += "%d" % classes + "\n"
-    return ret
-# }}}
 
-def extract_last_features(symbol, dates, close_prices, window): #{{{
-    assert(len(dates) == len(close_prices))
-    ret = ""
-    ret += symbol + ","
-    ret += str(dates[-1]) + ","
-    for i in range(len(close_prices)-window-1, len(close_prices)-1):
-        inc = close_prices[i+1]*1.0/close_prices[i]
-        if i != (len(close_prices)-2):
+class ExtractorBase:
+    def __init__(self, symbol, dates, open_prices, high_prices, low_prices, close_prices, window):
+        self.symbol = symbol
+        self.dates = dates
+        self.open_prices = open_prices
+        self.high_prices = high_prices
+        self.low_prices = low_prices
+        self.close_prices = close_prices
+        self.window = window
+    def extract_features_and_classes(self):
+        assert(False)
+    def extract_last_features(self):
+        assert(False)
+class Extractor1(ExtractorBase):
+    def extract_features_and_classes(self): #{{{
+        ret = ""
+        for i in range(len(self.close_prices)-self.window-1):
+            for  j in range(self.window):
+                inc = self.close_prices[i+j+1] * 1.0 / self.close_prices[i+j]
+                ret +=  str(inc) + ","
+            classes = 0
+            if self.close_prices[i+self.window + 1] > self.close_prices[i+self.window] :
+                 classes = 1
+            ret += "%d" % classes + "\n"
+        return ret
+    # }}}
+
+    def extract_last_features(self): #{{{
+        assert(len(self.dates) == len(self.close_prices))
+        ret = ""
+        ret += self.symbol + ","
+        ret += str(self.dates[-1]) + ","
+        for i in range(len(self.close_prices)-self.window-1, len(self.close_prices)-1):
+            inc = self.close_prices[i+1]*1.0/self.close_prices[i]
+            if i != (len(self.close_prices)-2):
+                ret += str(inc) + "," 
+            else:
+                ret += str(inc) + "\n" 
+        return ret
+    # }}}
+        
+class Extractor2(ExtractorBase):
+    def extract_features_and_classes(self): #{{{
+        ret = ""
+        for i in range(len(self.close_prices)-self.window-1):
+            for  j in range(self.window):
+                inc = self.close_prices[i+j+1] * 1.0 / self.close_prices[i+0]
+                ret +=  str(inc) + ","
+            classes = 0
+            if self.close_prices[i+self.window + 1] > self.close_prices[i+self.window] :
+                 classes = 1
+            ret += "%d" % classes + "\n"
+        return ret
+    # }}}
+
+    def extract_last_features(self): #{{{
+        assert(len(self.dates) == len(self.close_prices))
+        ret = ""
+        ret += self.symbol + ","
+        ret += str(self.dates[-1]) + ","
+        for i in range(len(self.close_prices)-self.window-1, len(self.close_prices)-1):
+            inc = self.close_prices[i+1]*1.0/self.close_prices[len(self.close_prices)-self.window-1]
+            if i != (len(self.close_prices)-2):
+                ret += str(inc) + "," 
+            else:
+                ret += str(inc) + "\n" 
+        return ret
+    # }}}
+
+class Extractor3(ExtractorBase):
+    def extract_features_and_classes(self): #{{{
+        ret = ""
+        for i in range(len(self.close_prices)-self.window-1):
+            for  j in range(self.window):
+                inc = self.open_prices[i+j+1] * 1.0 / self.close_prices[i]
+                
+                ret +=  str(inc) + ","
+                inc = self.high_prices[i+j+1] * 1.0 / self.close_prices[i]
+                
+                ret +=  str(inc) + ","
+                inc = self.low_prices[i+j+1] * 1.0 / self.close_prices[i]
+                
+                ret +=  str(inc) + ","
+                inc = self.close_prices[i+j+1] * 1.0 / self.close_prices[i+0]
+                ret +=  str(inc) + ","
+            classes = 0
+            if self.close_prices[i+self.window + 1] > self.close_prices[i+self.window] :
+                 classes = 1
+            ret += "%d" % classes + "\n"
+        return ret
+    # }}}
+
+    def extract_last_features(self): #{{{
+        assert(len(self.dates) == len(self.close_prices))
+        ret = ""
+        ret += self.symbol + ","
+        ret += str(self.dates[-1]) + ","
+        for i in range(len(self.close_prices)-self.window-1, len(self.close_prices)-1):
+            inc = self.open_prices[i+1]*1.0/self.close_prices[len(self.close_prices)-self.window-1]
             ret += str(inc) + "," 
-        else:
-            ret += str(inc) + "\n" 
-    return ret
-# }}}
+            inc = self.high_prices[i+1]*1.0/self.close_prices[len(self.close_prices)-self.window-1]
+            ret += str(inc) + "," 
+            inc = self.low_prices[i+1]*1.0/self.close_prices[len(self.close_prices)-self.window-1]
+            ret += str(inc) + "," 
+            inc = self.close_prices[i+1]*1.0/self.close_prices[len(self.close_prices)-self.window-1]
+            if i != (len(self.close_prices)-2):
+                ret += str(inc) + "," 
+            else:
+                ret += str(inc) + "\n" 
+        return ret
+    # }}}
 
 def main(options, args): # {{{
     #cmd_str = "/bin/mkdir -p " + options.output
@@ -57,6 +143,8 @@ def main(options, args): # {{{
     f_train = open(options.output + "/" + "train.csv", "w")
     f_last = open(options.output + "/" + "last.csv", "w")
 
+    # get the extractor
+    Extractor = globals()[options.extractor]
     file_list = get_file_list(options.stocks_path)
     stock_num = 0
     for f in file_list:
@@ -65,13 +153,14 @@ def main(options, args): # {{{
             logging.debug("build the %d's stock" % stock_num)
         symbol = get_stock_from_path(f)
         dates, open_prices, high_prices, low_prices, close_prices, adjust_close_prices, volumes = get_stock_data(f)
+        extractor = Extractor(symbol, dates, open_prices, high_prices, low_prices, close_prices, options.window)
         if len(dates)  < options.limit:
             logging.debug("%s is too short(%d)!" % (symbol, len(dates)))
             continue
         print >> f_train, "%s" %  \
-            extract_features_and_classes(close_prices, options.window),
+            extractor.extract_features_and_classes(),
         print >> f_last, "%s" % \
-                extract_last_features(symbol, dates, close_prices, options.window),
+                extractor.extract_last_features(),
     f_train.close()
     f_last.close()
 # }}}
@@ -80,7 +169,9 @@ def parse_options(parser): #{{{
     """
     parser command line
     """
-    parser.add_option("--window", dest="window",action = "store", \
+    parser.add_option("--extractor", dest="extractor",action = "store", \
+            default="Extractor1", help = "the extractor to use")
+    parser.add_option("--window", type="int", dest="window",action = "store", \
             default=60, help = "the history price window")
     parser.add_option("--output", dest="output",action = "store", \
             default=local_path + "/data/prices_series/", help = "the output directory")
--- strategy_mining/model_build_price_series2_test.py
+++ strategy_mining/model_build_price_series2_test.py
@@ -6,6 +6,7 @@ local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
 sys.path.append(local_path + "/./")
 
 import model_build_price_series2 as model
+from model_build_price_series2 import *
 import unittest
 from optparse import OptionParser
 
@@ -20,56 +21,87 @@ class ModuleBuildPriceSeries2Test (unittest.TestCase):
         parser = OptionParser()
         (options, args) = model.parse_options(parser)
         self.assertEqual(60, options.window)
-        self.assertEqual('data/prices_series/', options.output)
+        self.assertEqual(local_path + '/data/prices_series/', options.output)
+        self.assertEqual('Extractor1', options.extractor)
     # }}}
 
     def test_extract_features_and_classes_1(self): # {{{
         close_prices = [1,2,4,2]
+        extractor = Extractor1("abc",None, close_prices, close_prices, close_prices, close_prices, 2)
         self.assertEqual("2.0,2.0,0\n",
-                model.extract_features_and_classes(close_prices, 2) )
+                extractor.extract_features_and_classes() )
     # }}}
 
     def test_extract_features_and_classes_2(self): # {{{
         close_prices = [1,2,4,2,4]
+        extractor = Extractor1("abc",None, None, None, None, close_prices, 2)
         self.assertEqual("2.0,2.0,0\n" + 
                          "2.0,0.5,1\n",
-                model.extract_features_and_classes(close_prices, 2) )
+                extractor.extract_features_and_classes() )
     # }}}
 
     def test_extract_features_and_classes_3(self): # {{{
         close_prices = [1,2,4,2,1]
+        extractor = Extractor1("abc",None, None, None, None, close_prices, 2)
+
         self.assertEqual("2.0,2.0,0\n" + 
                          "2.0,0.5,0\n",
-                model.extract_features_and_classes(close_prices, 2) )
+                extractor.extract_features_and_classes() )
     # }}}
 
     def test_extract_features_and_classes_4(self): # {{{
         close_prices = [1,2,4,2,1]
+        extractor = Extractor1("abc",None, None, None, None, close_prices, 2)
         self.assertEqual("2.0,2.0,0\n" + 
                          "2.0,0.5,0\n",
-                model.extract_features_and_classes(close_prices, 2) )
+                extractor.extract_features_and_classes() )
     # }}}
 
     def test_extract_features_and_classes_5(self): # {{{
         close_prices = [1,2,4,2,1,2,4]
+        extractor = Extractor1("abc",None, None, None, None, close_prices, 4)
         self.assertEqual("2.0,2.0,0.5,0.5,1\n" + 
                          "2.0,0.5,0.5,2.0,1\n",
-                model.extract_features_and_classes(close_prices, 4) )
+                extractor.extract_features_and_classes() )
     # }}}
 
-    def test_extract_features_and_classes_5(self): # {{{
+    def test_extract_features_and_classes_6(self): # {{{
         close_prices = [1,2,4,2,1,2,4]
         dates = [1,2,3,4,5,6,7]
+        extractor = Extractor1("SYM",dates, None, None, None, close_prices, 4)
         self.assertEqual("SYM,7,0.5,0.5,2.0,2.0\n",
-                model.extract_last_features("SYM", dates, close_prices, 4) )
+                extractor.extract_last_features() )
     # }}}
 
-    def test_extract_features_and_classes_5(self): # {{{
+    def test_extract_features_and_classes_7(self): # {{{
         close_prices = [1,2,4,2,1,2,4]
         dates = [1,2,3,4,5,6,7]
+        extractor = Extractor1("SYM",dates, None, None, None, close_prices, 4)
         self.assertEqual("SYM,7,0.5,0.5,2.0,2.0\n",
-                model.extract_last_features("SYM", dates, close_prices, 4) )
+                extractor.extract_last_features() )
     # }}}
+
+    def test_extract_2(self):
+        close_prices = [1,2,4,2,1,2,4]
+        dates = [1,2,3,4,5,6,7]
+        extractor = Extractor2("SYM", dates, None, None, None, close_prices, 4)
+        self.assertEqual("2.0,4.0,2.0,1.0,1\n" + 
+                         "2.0,1.0,0.5,1.0,1\n",
+                         extractor.extract_features_and_classes() )
+        self.assertEqual("SYM,7,0.5,0.25,0.5,1.0\n",
+                extractor.extract_last_features() )
+    def test_extract_2(self):
+        open_prices =  [1,2,4,2,1,2,4]
+        high_prices =  [1,2,4,2,1,2,4]
+        low_prices =   [1,2,4,2,1,2,4]
+        close_prices = [1,2,4,2,1,2,4]
+        dates = [1,2,3,4,5,6,7]
+        extractor = Extractor3("SYM", dates, open_prices, high_prices, low_prices, close_prices, 4)
+        self.assertEqual("2.0,2.0,2.0,2.0,4.0,4.0,4.0,4.0,2.0,2.0,2.0,2.0,1.0,1.0,1.0,1.0,1\n" + 
+                         "2.0,2.0,2.0,2.0,1.0,1.0,1.0,1.0,0.5,0.5,0.5,0.5,1.0,1.0,1.0,1.0,1\n",
+                         extractor.extract_features_and_classes() )
+        self.assertEqual("SYM,7,0.5,0.5,0.5,0.5,0.25,0.25,0.25,0.25,0.5,0.5,0.5,0.5,1.0,1.0,1.0,1.0\n",
+                extractor.extract_last_features() )
 
     def test_main_1(self): # {{{
         parser = OptionParser()
