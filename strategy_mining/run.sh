a3d2e628112f81c7a6b7477282f6d57c25ae8e9f
Red Bin <binred@outlook.com>
2014-11-16 23:12:50 +0800

    xxxx

---
+++ strategy_mining/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh 
+######################### 
+#@author binred@outlook.com
+#@date 
+#@desc TODO 
+######################### 
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export SCRIPT_PATH=`dirname $(readlink -f $0)` # get the path of the script 
+pushd . > /dev/null 
+
+cd "$SCRIPT_PATH" 
+ 
+cd ../tools/ ; 
+python daily_bars_getter.py
+
+cd "$SCRIPT_PATH" 
+python model_build_price_series2.py --extractor=Extractor5 --output=data/price_series5/ --window=14
+python model_tuner.py --input=data/price_series5/ 
+
+popd  > /dev/null # return the directory orignal
+
