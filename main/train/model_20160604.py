c673da390e996308599a228c5ccf62028da068ac
U-DESKTOP-PP9PIE6\abin <you@example.com>
2016-06-04 16:42:20 +0800

    xxxxx

--- main/train/model_20160604.py
+++ main/train/model_20160604.py
@@ -4,11 +4,17 @@
 #@author  Bin Hong
 
 import sys,os
-from sklearn.ensemble import GradientBoostingClassifier
 local_path = os.path.dirname(__file__)
 root = os.path.join(local_path, '..')
 sys.path.append(root)
 
 import train.model_param_set as params
 
-l_params = params.d_all.keys()
+l_params = [
+        "ta1_GBCv1n1000md3_l3_s2000e2009",
+        "ta1_GBCv1n1000md3_l3_s2001e2010",
+        "ta1_GBCv1n1000md3_l3_s2002e2011",
+        "ta1_GBCv1n1000md3_l3_s2003e2012",
+        "ta1_GBCv1n1000md3_l3_s2004e2013",
+        "ta1_GBCv1n1000md3_l3_s2005e2014",
+        ]
