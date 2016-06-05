71a225fe6c92dbdd1330f25f3f43c534c9c3c037
U-DESKTOP-PP9PIE6\abin <you@example.com>
2016-06-05 13:09:44 +0800

    xxxx

---
+++ main/utils/__init__.py
@@ -0,0 +1,13 @@
+from functools import wraps 
+import time
+def time_me(function):  
+    @wraps(function)  
+    def function_timer(*args, **kwargs):  
+        t0 = time.time()  
+        result = function(*args, **kwargs)  
+        t1 = time.time()  
+        print ("Total time running %s: %s seconds" %  
+               (function.func_name, str(t1-t0))  
+               )  
+        return result  
+    return function_timer 
