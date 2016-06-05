e07e16a5ef1ef484c9d1e900d6427dce57850863
U-DESKTOP-PP9PIE6\abin <you@example.com>
2016-06-05 15:29:20 +0800

    xxxx

--- main/cross/cross_diff.py
+++ main/cross/cross_diff.py
@@ -26,37 +26,36 @@ def accu(df, label, threshold):
     npTrue = npLabel[npLabel > 1.0]
     return {"pos": npPos.size, "trueInPos":npTrueInPos.size}
 
-@time_me
 def get_df(f):
-    with open(f) as ff:
+    with open(f, "rb") as ff:
         df = pkl.load(ff)
     #return joblib.load(f)
     #return pd.read_csv(f)
     return df
 
-@time_me
 def get_range(df, start ,end):
     return df.query('date >="%s" & date <= "%s"' % (start, end)) 
 def main(argv):
     conf_file = argv[1]
     impstr = "import %s as conf" % conf_file
     exec impstr
+    out_file = os.path.join(root, 'data', "crosses", conf_file+".report")
+    fout = open(out_file, 'w')
 
     dfAll = None
     sym2ta = None
     for each in conf.l_params:
-        print each
+        print >> fout, each,
         merged_file = os.path.join(each[1], "merged.pkl")
         df = get_df(merged_file)
         df = get_range(df, each[3][0], each[3][1])
         cls = joblib.load(os.path.join(root, 'data', 'models',"model_" + each[0]+ ".pkl"))
-        
         feat_names = model.get_feat_names(df)
         npFeat = df.loc[:,feat_names].values
         npPred = cls.predict_proba(npFeat)[:,1]
         df["pred"] = npPred
         dacc =  accu(df, each[2], each[4])
-        print dacc["trueInPos"], dacc["pos"], dacc["trueInPos"]*1.0 / dacc["pos"]
+        print>> fout, dacc["trueInPos"], dacc["pos"], dacc["trueInPos"]*1.0 / dacc["pos"]
 
 if __name__ == '__main__':
     main(sys.argv)
