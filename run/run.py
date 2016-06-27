4c23de226532fa48ad1aea5352d07400dfcfa686
hongbin <hongbin0908@126.com>
2016-06-27 19:30:24 +0800

    xxxx

--- run/run.py
+++ run/run.py
@@ -20,7 +20,7 @@ import main.base as base
 @time_me
 def main(argv):
     yeod.main(["dow", 5])
-    build.main(["dow", 'call1',5])
+    build.main(["dow", 'call1s1',5])
     last_date = base.last_trade_date()
     pred.main(['call1s1_dow_GBCv1n322md3lr001_l5_s1700e2009', 'call1s1_dow', last_date, last_date, 'label5'])
 if __name__ == '__main__':
