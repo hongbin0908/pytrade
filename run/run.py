11a2b831f812247e75e80b9dc04641b57c4a1dc6
hongbin <hongbin0908@126.com>
2016-07-02 12:44:33 +0800

    add cross_tail_stage_conf

--- run/run.py
+++ run/run.py
@@ -23,5 +23,6 @@ def main(argv):
     build.main(["dow", 'call1s1',5])
     last_date = base.last_trade_date()
     pred.main(['call1s1_dow_GBCv1n322md3lr001_l5_s1700e2009', 'call1s1_dow', last_date, last_date, 'label5'])
+    pred.main(['call1s1_dow_GBCv1n322md3lr001_l5_s1700e2009', 'call1s1_sp500', last_date, last_date, 'label5'])
 if __name__ == '__main__':
     main(sys.argv[1:])
