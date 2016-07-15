c22fcd6e904fcd6e095056f609a381fad6d0f6bc
hongbin <you@example.com>
2016-07-16 02:52:17 +0800

    xxxxx

---
+++ run/batch_sp500Top100_100.sh
@@ -0,0 +1,3 @@
+./main/yeod/yeod_b.py sp500Top100 100 10
+./main/ta/build_b.py sp500Top100 100 call1s1 10
+./main/model/model_work_b.py  call1s1-sp500Top100  100  GBCv1n3000md3lr001 label5 1700-01-01 2009-12-31 0 0 10
