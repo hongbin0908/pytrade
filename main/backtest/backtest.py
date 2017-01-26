579fc2b3be67bb4ae0095800c60dfa9a123bb783
hongbin <you@example.com>
2017-01-26 10:19:05 +0800

    xxxx

--- main/backtest/backtest.py
+++ main/backtest/backtest.py
@@ -2,11 +2,15 @@ import os
 import sys
 import pandas as pd
 import bt
+import matplotlib
+matplotlib.use('Agg')
 from bt.algos import SelectWhere
 
 local_path = os.path.dirname(__file__)
 root = os.path.join(local_path, '..', '..')
 sys.path.append(root)
+
+from main import base
 
 def f(x):
     if x[0]:
@@ -14,8 +18,23 @@ def f(x):
     print(x)
     return x
 
+def transfer():
+    df_pred = pd.read_pickle(os.path.join(root, "data", "cross", "pred%s.pkl" % base.last_trade_date()))
+    df_pred = df_pred[df_pred.date >= "2015-01-01"]
+    index = df_pred[df_pred.sym == "MSFT"]["date"].unique(); index.sort()
+    columns = df_pred["sym"].unique(); columns.sort()
+    df_price = pd.DataFrame(index = index, columns = columns)
+    df_thred = pd.DataFrame(index = index, columns = columns)
 
-
+    for i, each in df_pred.iterrows():
+        df_price.loc[each["date"], each["sym"]] = each["close"]
+        df_thred.loc[each["date"], each["sym"]] = each["pred"]
+    # assert
+    for sym in df_price.columns():
+        s = df_price[sym]
+        
+        
+     
 def run(pred_file):
     # df = pd.read_pickle(os.path.join(root, "data", "cross", "pred2017-01-09.pkl"))
     df = pd.read_pickle(pred_file)
@@ -54,3 +73,5 @@ def run(pred_file):
     fig.savefig(os.path.join(root, "report", "backtest.png"))
     #res.display()
 
+if __name__ == '__main__':
+    transfer()
