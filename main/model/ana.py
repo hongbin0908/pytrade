7ec29e19988c6932d1601df3f843091a5d378c2e
Bin Hong <hongbin0908@126.com>
2017-03-22 11:30:51 +0800

    xxxx

--- main/model/ana.py
+++ main/model/ana.py
@@ -78,8 +78,8 @@ def roi_level(df, score, max_hold_num=-1):
     index = ["top", "roi", "threshold"]
     res = pd.DataFrame(data=None, columns=index)
     for top in [1000, 5000, 10000, 100000]:
-        roi, num = roi(df.head(top), score, max_hold_num)
-        res = res.append(pd.Series((top, num, roi, float(df.head(top).tail(1)["pred"].values)), index=index), ignore_index=True)
+        r, num = roi(df.head(top), score, max_hold_num)
+        res = res.append(pd.Series((top, num, r, float(df.head(top).tail(1)["pred"].values)), index=index), ignore_index=True)
 
 
     res = res.append(pd.Series(("total", accurate(df,score), 0.5),index=index), ignore_index=True)
