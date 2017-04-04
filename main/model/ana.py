009074c3338794a3325432e3a416893d58f19737
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-04-04 11:37:45 +0800

    xxxx

--- main/model/ana.py
+++ main/model/ana.py
@@ -87,9 +87,9 @@ def roi2(df, score, max_hold_num=-1):
     res = float(profile.sum())
     return res
 
-def roi(df, score, max_hold_num=-1, threshold=2):
+def roi(df, score, max_hold_num=-1, threshold=3):
     if max_hold_num > 0:
-        df = df.groupby('date').filter(lambda x: len(x)>=2).groupby('date').head(max_hold_num)
+        df = df.groupby('date').filter(lambda x: len(x)>=threshold).groupby('date').head(max_hold_num)
     num = 1000/df.loc[:, "close"]
     nValue = df.loc[:, "close"]*df.loc[:,score.get_name()]*num
     profile = nValue - 1000
