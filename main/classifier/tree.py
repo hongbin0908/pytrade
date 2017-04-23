770678bff1b5e1b6a84ea0845a460cfa16edb92a
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-04-24 02:09:52 +0800

    xxxxxxxxxx

--- main/classifier/tree.py
+++ main/classifier/tree.py
@@ -159,13 +159,11 @@ class ccl2(BaseClassifier):
         self.classifier.add(LSTM(input_shape=(2, X.shape[2]),  output_dim =8,
                                  return_sequences = True, kernel_initializer=initializers.glorot_normal(123) ))
         self.classifier.add(Flatten())
-        #self.classifier.add(Activation('linear'))
         self.classifier.add(Activation('relu'))
-        self.classifier.add(Dense( output_dim=8, kernel_initializer=initializers.glorot_normal(123)))
-        #self.classifier.add(Activation('linear'))
+        self.classifier.add(Dense( output_dim=4, kernel_initializer=initializers.glorot_normal(123)))
         self.classifier.add(Activation('relu'))
         self.classifier.add(Dropout(0.3, seed=123))
-        self.classifier.add(Dense(output_dim=8))
+        self.classifier.add(Dense(output_dim=4))
         self.classifier.add(Activation('tanh'))
         self.classifier.add(Dense(output_dim=1, kernel_initializer=initializers.glorot_normal(123)))
         self.classifier.add(Activation('sigmoid'))
@@ -173,50 +171,6 @@ class ccl2(BaseClassifier):
         opt = Adam(lr=2e-5)
 
         self.classifier.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
-        self.classifier.fit(X, y, validation_data=(X_t, y_t), batch_size=self.batch_size, nb_epoch=self.nb_epoch)
-    def predict_proba(self, X):
-        X = self.transfer_shape(X)
-        re = self.classifier.predict_proba(X)
-        re = np.hstack([1-re, re])
-        return re
-class ccl(BaseClassifier):
-    def __init__(self, batch_size = 32, nb_epoch=10):
-        model = Sequential()
-        self.classifier = model
-        self.batch_size = batch_size
-        self.nb_epoch = nb_epoch
-        pass
-    def get_name(self):
-        return "ccl-%d" % (self.nb_epoch)
-
-    def transfer_shape(self,X):
-        return d2tod3(X, window=2)
-        return np.reshape(X, (X.shape[0], 1, X.shape[1]))
-
-    def fit(self, X, y, X_t, y_t):
-        X = self.transfer_shape(X)
-        X_t = self.transfer_shape(X_t)
-        y = y[2-1:]
-        y_t = y_t[2-1:]
-        #self.classifier.add(Dense(500, input_shape=( X.shape[1],)))
-        self.classifier.add(LSTM(input_shape=(2, X.shape[2]),  output_dim =8,
-                                 return_sequences = True ))
-        self.classifier.add(Flatten())
-        #self.classifier.add(Activation('linear'))
-        self.classifier.add(Activation('relu'))
-        self.classifier.add(Dense( output_dim=4))
-        self.classifier.add(Activation('relu'))
-        self.classifier.add(Dropout(0.3, seed=7))
-        self.classifier.add(Dense(output_dim=4))
-        self.classifier.add(Activation('relu'))
-        self.classifier.add(Dropout(0.3, seed=14))
-        self.classifier.add(Dense( output_dim=4))
-        self.classifier.add(Activation('relu'))
-        self.classifier.add(Dropout(0.3, seed=7))
-        self.classifier.add(Dense(output_dim=1))
-        self.classifier.add(Activation('sigmoid'))
-        sgd = SGD(lr=0.01)
-        self.classifier.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
         self.classifier.fit(X, y, validation_data=(X_t, y_t), batch_size=self.batch_size, nb_epoch=self.nb_epoch)
     def predict_proba(self, X):
         X = self.transfer_shape(X)
