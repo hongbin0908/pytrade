6a7326f773cc6c82d6318cb0624cd2fe5f56bd61
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-04-30 09:59:09 +0800

    xxxx

--- main/classifier/logit.py
+++ main/classifier/logit.py
@@ -54,6 +54,10 @@ sys.path.append(root)
 def binary_accuracy(y_true, y_pred):
     return K.mean(K.equal(y_true, K.round(y_pred)), axis=-1)
 
+def top_accuracy(y_true, y_pred):
+    K.in_top_k(y_pred, y_true)
+    return K.mean(K.equal(y_true, K.round(y_pred)), axis=-1)
+
 class Logit(BaseClassifier):
     def __init__(self, batch_size = 100, nb_epoch=30, verbose = 1):
         model = Sequential()
@@ -65,7 +69,10 @@ class Logit(BaseClassifier):
     def get_name(self):
         return "ccl-logit-%d-%d" % (self.nb_epoch, self.batch_size)
     def fit(self, X, y, X_t, y_t):
-        self.classifier.add(Dense(input_dim=X.shape[1], output_dim=1))
+        self.classifier.add(Dense(input_dim=X.shape[1], output_dim=32))
+        self.classifier.add(Activation('relu'))
+        self.classifier.add(Dropout(0.5))
+        self.classifier.add(Dense(output_dim=1))
         self.classifier.add(Activation('sigmoid'))
         sgd = SGD(lr=0.01)
         opt = Adam(lr=4e-5)
