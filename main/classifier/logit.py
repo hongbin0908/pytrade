4ca0386e6fffb4b2d7a775698d3a884d9972d571
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-04-30 11:37:46 +0800

    xxxx

--- main/classifier/logit.py
+++ main/classifier/logit.py
@@ -111,7 +111,7 @@ class Logit(BaseClassifier):
     def get_name(self):
         return "ccl-logit-%d-%d" % (self.nb_epoch, self.batch_size)
     def fit(self, X, y, X_t, y_t):
-        self.classifier.add(Dense(input_dim=X.shape[1], output_dim=8))
+        self.classifier.add(Dense(input_dim=X.shape[1], output_dim=32))
         self.classifier.add(Activation('relu'))
         self.classifier.add(Dropout(0.5))
         for i in range(1):
