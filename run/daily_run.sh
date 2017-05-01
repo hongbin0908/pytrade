9a04be5d4576b96cc7eddeb5ef4b128d5517eb46
Bin Hong <hongbin0908@126.com>
2017-05-01 16:10:18 +0800

    add report

--- run/daily_run.sh
+++ run/daily_run.sh
@@ -12,5 +12,7 @@ cd "$SCRIPT_PATH"
 python3 ${SCRIPT_PATH}/get_yeod.py
 rsync -avH /data/users/hongbin/pytrade hongbin@bd-r1hdp22:~/
 ssh hongbin@bd-r1hdp22 "/usr/bin/python3 /data/users/hongbin/pytrade/run/run.py  -f"
+rsync -avH hongbin@bd-r1hdp22:/data/users/hongbin/pytrade/data/report /data/users/hongbin/pytrade/data/
+rsync -e "ssh -p 443" -avH /data/users/hongbin/pytrade/data/report hongbin@hongindex.com:~/misc/nginx/html/
 
 popd   > /dev/null # return the directory orignal
