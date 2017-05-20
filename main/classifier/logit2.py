e5d35935a50d0cc5e266b1c6cdd0fa1593b9f1ed
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-05-20 23:08:19 +0800

    xxxxx

--- main/classifier/logit2.py
+++ main/classifier/logit2.py
@@ -28,7 +28,7 @@ class Logit2(BaseClassifier):
         return "ccl-logit-%d-%d" % (self.nb_epoch, self.batch_size)
     def fit(self, X, y, df_test, score):
         self.classifier.add(Dense(input_dim=X.shape[1], output_dim=64,
-                                  kernel_initializer=keras.initializers.glorot_normal(seed=570255),
+                                  kernel_initializer=keras.initializers.glorot_uniform(seed=570255),
                                   bias_initializer=keras.initializers.constant(0.0)))
         self.classifier.add(Activation('relu'))
         #self.classifier.add(Dropout(0.5, seed=969458))
@@ -37,7 +37,7 @@ class Logit2(BaseClassifier):
         #    self.classifier.add(Activation('relu'))
         #    self.classifier.add(Dropout(0.5 ,seed=14306))
 
-        self.classifier.add(Dense(output_dim=1, kernel_initializer=keras.initializers.glorot_normal(seed=447630),
+        self.classifier.add(Dense(output_dim=1, kernel_initializer=keras.initializers.glorot_uniform(seed=447630),
                                   bias_initializer=keras.initializers.constant(0.0)))
         self.classifier.add(Activation('sigmoid'))
         opt = SGD(lr=0.01)
