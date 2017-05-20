d705d1af7ad691229ab843d32fdf6e353ca415a9
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-05-21 00:07:54 +0800

    xxxxx

--- main/classifier/interval_acc.py
+++ main/classifier/interval_acc.py
@@ -70,7 +70,7 @@ class IntervalAcc(Callback):
 
     def on_epoch_end(self, epoch, logs={}):
         print(epoch, self.interval)
-        if epoch % self.interval*5 != 0:
+        if epoch % (self.interval*5) != 0:
             return
         print("")
         print("LONG...")
