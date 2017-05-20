c2569df38d4f10a075c72db082495ef9080d654e
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-05-20 15:17:46 +0800

    xxxxx

--- main/base/stock_fetcher.py
+++ main/base/stock_fetcher.py
@@ -24,7 +24,7 @@ def get_stock(symbol):
 
         url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=%s&api_key=77sr5UvZ2qs5z38i_Hf5' % symbol
         response = urllib.request.urlopen(url)
-        response = response.read()
+        response = response.read().decode('utf8')
         response = json.loads(response)
         try:
             df = pd.DataFrame(response['datatable']['data'])
