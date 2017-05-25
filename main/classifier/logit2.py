9c247e33354968457b44c3106214c027f67f39b9
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-05-25 20:57:20 +0800

    让模型稳定

--- main/classifier/logit2.py
+++ main/classifier/logit2.py
@@ -33,11 +33,11 @@ class Logit2(BaseClassifier):
                                   kernel_initializer=keras.initializers.glorot_uniform(seed=570255),
                                   bias_initializer=keras.initializers.constant(0.0)))
         self.classifier.add(Activation('relu'))
-        self.classifier.add(Dropout(0.5, seed=969458))
+        #self.classifier.add(Dropout(0.5, seed=969458))
         for i in range(2):
             self.classifier.add(Dense(output_dim=64, kernel_initializer=keras.initializers.glorot_normal(seed=846635)))
             self.classifier.add(Activation('relu'))
-            self.classifier.add(Dropout(0.5 ,seed=14306))
+            #self.classifier.add(Dropout(0.5 ,seed=14306))
 
         self.classifier.add(Dense(output_dim=1, kernel_initializer=keras.initializers.glorot_uniform(seed=447630),
                                   bias_initializer=keras.initializers.constant(0.0)))
