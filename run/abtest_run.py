42bd79c7fd56c3c915fa3e2c43a9ecea36804818
Bin Hong <abin@workdeMacBook-Pro.local>
2017-05-31 14:52:42 +0800

    test abtest_run

--- run/abtest_run.py
+++ run/abtest_run.py
@@ -111,6 +111,7 @@ if __name__ == '__main__':
                 score_build.work(confer)
                 bitlize.work(confer)
                 selected.work(confer)
+                print(confer.get_classifier_file())
                 model.work(confer)
                 pd.set_option('display.expand_frame_repr', False)
                 pd.options.display.max_rows = 999
