8df0fc08b434e9e74b6ba55f97fd9f1810428f35
Bin Hong <abin@workdeMacBook-Pro.local>
2017-06-01 16:08:08 +0800

    abtest adj

--- run/abtest_run.py
+++ run/abtest_run.py
@@ -4,13 +4,7 @@
 
 import sys
 import os
-import numpy as np
 import pandas as pd
-import matplotlib
-matplotlib.use('Agg')
-np.random.seed(123)
-import tensorflow as tf
-tf.set_random_seed(123)
 
 local_path = os.path.dirname(__file__)
 root = os.path.join(local_path, '..')
@@ -21,59 +15,14 @@ from main.work import build
 from main.work import score as score_build
 from main.work import bitlize
 from main.work import selected
-from main.work import report
 from main.work import abtest_report
 from main.model import ana
 from main import base
-from main.classifier.tree import cnn
-from main.classifier.tree import ccl2
-from main.classifier.tree import RFCv1n2000md3msl100
-from main.classifier.tree import MyMdnClassifier
-from main.classifier.ts import Ts
 from main.classifier.logit2 import Logit2
-from main.classifier.logit import Logit
 from main.work.conf import MyConfStableLTa
-from main.work.conf import MyConfForTest
-
-from main.ta import ta_set
-from keras.metrics import top_k_categorical_accuracy
-
-
-def get_confs2():
-    score = 5
-    return [
-        MyConfStableLTa(classifier=Ts(max_iterations=20000), score=score),
-    ]
-def get_confs1():
-    score = 5
-    return [
-        MyConfStableLTa(classifier=cnn(nb_epoch=5), score=score),
-        MyConfStableLTa(classifier=cnn(),score=score),
-        MyConfStableLTa(classifier=cnn(num_filt_2=4), score=score),
-        MyConfStableLTa(classifier=cnn(num_filt_1=6, num_filt_2=4), score=score),
-        MyConfStableLTa(classifier=cnn(batch_size=1000), score=score),
-    ]
-
-def get_confs2():
-    score = 5
-    return [
-        MyConfStableLTa(classifier=ccl2(batch_size=32, nb_epoch=10), score=score),
-    ]
-def get_confs():
-    score = 5
-    return [
-        #MyConfStableLTa(classifier=ccl2(batch_size=32, nb_epoch=20), score=score),
-        #MyConfStableLTa(classifier=cnn(batch_size=32, nb_epoch=20), score=score),
-        MyConfStableLTa(classifier=Logit2(), score=score),
-    ]
-def get_test_confs():
-    score = 5
-    return [
-        MyConfForTest()
-    ]
 
 if __name__ == '__main__':
-    iter_num = 1
+    iter_num = 2
     abtest_models = {
         #"Logit10":Logit2(nb_epoch=10),
         "Logit20":Logit2(nb_epoch=20),
