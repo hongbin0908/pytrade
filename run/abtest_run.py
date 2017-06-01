6cb70197f96d92698554c48678a91ca3092c242e
Bin Hong <abin@workdeMacBook-Pro.local>
2017-06-01 16:43:23 +0800

    abtest adj

--- run/abtest_run.py
+++ run/abtest_run.py
@@ -22,7 +22,7 @@ from main.classifier.logit2 import Logit2
 from main.work.conf import MyConfStableLTa
 
 if __name__ == '__main__':
-    iter_num = 2
+    iter_num = 3
     abtest_models = {
         #"Logit10":Logit2(nb_epoch=10),
         "Logit20":Logit2(nb_epoch=20),
