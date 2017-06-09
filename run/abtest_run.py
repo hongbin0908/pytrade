b8262ff629da51fcddb8907085c07bdaddfba9c3
Bin Hong <abin@workdeMacBook-Pro.local>
2017-06-09 12:52:22 +0800

    abtest

--- run/abtest_run.py
+++ run/abtest_run.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
     abtest_confs = {
         #"adj": MyConfStableLTa(classifier=Logit2(30), is_adj= True),
-        #"score5": MyConfStableLTa(classifier=Logit2(30), is_adj = False),
+        "score5": MyConfStableLTa(classifier=Logit2(30), is_adj = False),
         "score4": MyConfStableLTa(classifier=Logit2(30), is_adj = False, score=4),
         "score6": MyConfStableLTa(classifier=Logit2(30), is_adj = False, score=6),
         #"score2": MyConfStableLTa(classifier=Logit2(30), is_adj = False, score=2),
