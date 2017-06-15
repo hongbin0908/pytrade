5f01860c359f598c3751036b5fc63ae0326277da
Bin Hong <abin@workdeMacBook-Pro.local>
2017-06-15 15:00:54 +0800

    refact mdn model

--- run/abtest_run.py
+++ run/abtest_run.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
         "score5_30": MyConfStableLTa(classifier=Logit2(dim=30), is_adj = False),
         "score5_64": MyConfStableLTa(classifier=Logit2(dim=64), is_adj = False),
         "score5_8": MyConfStableLTa(classifier=Logit2(dim=8), is_adj = False),
-        "score5_8": MyConfStableLTa(classifier=Logit2(dim=16), is_adj = False),
+        "score5_16": MyConfStableLTa(classifier=Logit2(dim=16), is_adj = False),
         #"ccl2": MyConfStableLTa(classifier=Logit2(), is_adj = False),
         #"ccl3": MyConfStableLTa(classifier=Logit2(dim=10, hs=6), is_adj = False),
         #"delta": MyConfStableLTa(classifier=Logit(nb_epoch=10), is_adj = False),
