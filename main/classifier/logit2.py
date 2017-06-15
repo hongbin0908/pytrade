c74a95a83d1793052061934c33169eb285f9ec60
Bin Hong <abin@workdeMacBook-Pro.local>
2017-06-15 17:04:53 +0800

    refact mdn model

--- main/classifier/logit2.py
+++ main/classifier/logit2.py
@@ -17,12 +17,13 @@ from main.classifier.base_classifier import BaseClassifier
 from main.classifier.interval_acc import IntervalAcc
 
 class Logit2(BaseClassifier):
-    def __init__(self, dim = 64, hs = 3, batch_size = 100, nb_epoch=30, verbose = 1):
+    def __init__(self, dim = 64, hs = 3, batch_size = 100, nb_epoch=30, dropout=0.5, verbose = 1):
         self.batch_size = batch_size
         self.nb_epoch = nb_epoch
         self.verbose = verbose
         self.dim = dim
         self.hs = hs
+        self.dropout = dropout
         pass
     def get_name(self):
         return "ccl-logit-%d-%d-%d-%d" % (self.nb_epoch, self.batch_size, self.dim, self.hs)
@@ -35,11 +36,11 @@ class Logit2(BaseClassifier):
                                   kernel_initializer=keras.initializers.glorot_uniform(seed=570255),
                                   bias_initializer=keras.initializers.constant(0.0)))
         self.classifier.add(Activation('relu'))
-        self.classifier.add(Dropout(0.5, seed=969458))
+        self.classifier.add(Dropout(self.dropout, seed=969458))
         for i in range(self.hs):
             self.classifier.add(Dense(output_dim=self.dim, kernel_initializer=keras.initializers.glorot_normal(seed=846635)))
             self.classifier.add(Activation('relu'))
-            self.classifier.add(Dropout(0.5 ,seed=14306))
+            self.classifier.add(Dropout(self.dropout ,seed=14306))
 
         self.classifier.add(Dense(output_dim=1, kernel_initializer=keras.initializers.glorot_uniform(seed=447630),
                                   bias_initializer=keras.initializers.constant(0.0)))
