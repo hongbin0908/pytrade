b7f4eb3ee9b1c94f8b5275d05eec4efb4e21c94a
Bin Hong <abin@workdeMacBook-Pro.local>
2017-06-16 10:16:44 +0800

    refact mdn model

---
+++ main/classifier/logit3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+#@author  Bin Hong
+import numpy as np
+
+import keras
+from keras.layers import Activation, Dense, Dropout
+from keras.models import Sequential
+from keras import optimizers
+import os, sys
+
+local_path = os.path.dirname(__file__)
+root = os.path.join(local_path, '..', "..")
+sys.path.append(root)
+
+from main.classifier.base_classifier import BaseClassifier
+from main.classifier.logit2 import Logit2
+from main.classifier.interval_acc import IntervalAcc
+
+class Logit3(Logit2):
+    def __init__(self, dim = 64, hs = 3, batch_size = 100, nb_epoch=30, dropout=0.5, verbose = 1):
+        Logit2.__init__(dim, hs, batch_size, nb_epoch, dropout, verbose)
+        self.opt = optimizers.Adagrad(lr=4e-5)
