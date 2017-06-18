69887c840a88049082c0188e7e3c28f658b3437d
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-06-18 16:50:00 +0800

    xxxxx

--- main/dassert/dassert_score.py
+++ main/dassert/dassert_score.py
@@ -34,7 +34,9 @@ def work(confer):
     df2.reset_index(drop=True, inplace=True)
     assert len(df1) == len(df2)
     del df1["volume"]
+    del df1["volumeo"]
     del df2["volume"]
+    del df2["volumeo"]
     assert_frame_equal(df1, df2)
 
     #for sym in syms1:
