d6ac06a8dbcb81a1366a8af27a5c70184bcee8a5
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-06-18 21:29:51 +0800

    xxxxx

--- main/dassert/dassert_score.py
+++ main/dassert/dassert_score.py
@@ -45,8 +45,8 @@ def work(confer):
         df_s_2.reset_index(drop=True, inplace=True)
         df_s_2 = df_s_2.drop(df_s_2.tail(10).index)
         if sym == "ADI":
-            print(df_s_1[["date", "score_rel_5_0"]])
-            print(df_s_2[["date", "score_rel_5_0"]])
+            print(df_s_1[["date", "closeo", "score_rel_5_0"]])
+            print(df_s_2[["date", "closeo", "score_rel_5_0"]])
         assert_frame_equal(df_s_1[base.get_feat_score_names(df_s_1)],
                            df_s_2[base.get_feat_score_names(df_s_2)])
 
