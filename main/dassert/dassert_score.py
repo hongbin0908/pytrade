2a4edee5420171d23d9c69f6b04422c0cdfc9d40
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-06-18 16:48:14 +0800

    xxxxx

--- main/dassert/dassert_score.py
+++ main/dassert/dassert_score.py
@@ -33,6 +33,8 @@ def work(confer):
     df1.reset_index(drop=True, inplace=True)
     df2.reset_index(drop=True, inplace=True)
     assert len(df1) == len(df2)
+    del df1["volume"]
+    del df2["volume"]
     assert_frame_equal(df1, df2)
 
     #for sym in syms1:
