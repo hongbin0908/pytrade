13066ddddfb8c4f3eea409038384b6e199a55a84
U-LAPTOP-2BPV50N5\hongb <hongbin0908@126.com>
2017-06-18 19:45:20 +0800

    xxxxx

--- main/dassert/dassert_score.py
+++ main/dassert/dassert_score.py
@@ -43,7 +43,8 @@ def work(confer):
         df_s_2 = df2[df2.sym == sym]
         df_s_2.reset_index(drop=True, inplace=True)
         print(df_s_1.head(), df_s_2.head())
-        assert(len(df_s_1) == len(df_s_2))
+        print(df_s_1.score_rel_5_0)
+        print(df_s_2.score_rel_5_0)
         assert_frame_equal(df_s_1[base.get_feat_score_names(df_s_1)],
                            df_s_2[base.get_feat_score_names(df_s_2)])
 
